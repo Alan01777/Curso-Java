500c6cd324983bd1ec09b3ada4891a8b5bdb13b5
Alan01777 <camp.alan3@gmail.com>
2022-09-09 19:53:13 -0300

    exemplos

---
+++ exemplos/ex01.java
@@ -0,0 +1,17 @@
+package exemplos;
+
+public class ex01 {
+    public static void main(String[] args) {
+
+        int a = 3, b = 2;
+        int soma = a + b;
+        int multiplicacao = a * b;
+        int subtracao = a - b;
+        float divisao = (float) a / b;
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(divisao);
+        System.out.println(multiplicacao);
+    }
+}
